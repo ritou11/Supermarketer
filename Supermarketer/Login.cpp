880662f9cfc60d65635c08701cfe51992d03b12c
ritou11 <ritou11@gmail.com>
2017-06-03 09:43:04 +0800

    new project

---
+++ Supermarketer/Login.cpp
@@ -0,0 +1,14 @@
+#include "Login.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    Supermarketer::Login form;
+    Application::Run(%form);
+}
